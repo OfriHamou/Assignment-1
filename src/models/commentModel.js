8ade0c7a8e80de33b07b3871f49ce8027c7cec8c
ofrihamou <ofrihemo1@gmail.com>
2026-01-13 15:22:46 +0200

    Updating

---
+++ src/models/commentModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true 
+    },
+    sender: {
+        type: String,
+        required: true,
+    },
+    postId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+        required: true,
+    },
+}); 
