669fc5d51c6b9766f1818e190d6fe78da0544d73
Amit Eizenberg <amit.aizen@gmail.com>
2026-01-17 17:44:01 +0200

    Add get comment by ID endpoint
    - Added getCommentById function in commentController
    - Added GET /posts/comments/:id route

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -25,9 +25,23 @@ const getCommentsByPostId = async (req, res) => {
     }
 };
 
+const getCommentById = async (req, res) => {
+    try {
+        const comment = await Comment.findById(req.params.id);
+        if (!comment) {
+            return res.status(404).json({ message: 'Comment not found' });
+        }
+        res.status(200).json(comment);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
 
 
 module.exports = {
     createComment,
     getCommentsByPostId,
+    getCommentById,
 };
